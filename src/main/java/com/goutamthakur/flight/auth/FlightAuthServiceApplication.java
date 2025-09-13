324074cf6e7110952c6e8b73afbc4f05e113b6f8
goutamthakur <goutamthakur2508@gmail.com>
2025-09-13 23:35:15 +0530

    Initialize the project

---
+++ src/main/java/com/goutamthakur/flight/auth/FlightAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.goutamthakur.flight.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightAuthServiceApplication.class, args);
+	}
+
+}
