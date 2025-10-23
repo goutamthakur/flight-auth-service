dcea25bb0bf456e4dd6fb3cc1d92392cabda5285
goutamthakur <goutamthakur2508@gmail.com>
2025-10-23 23:07:49 +0530

    chore: updated the role entity to align with table

--- src/main/java/com/goutamthakur/flight/auth/domain/model/Role.java
+++ src/main/java/com/goutamthakur/flight/auth/domain/model/Role.java
@@ -1,27 +1,42 @@
 package com.goutamthakur.flight.auth.domain.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.Instant;
 
 @Entity
 @Table(name = "roles")
+@Getter
+@Setter
 public class Role {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(unique = true)
+    @Column(length = 50, unique = true, nullable = false)
     private String name;
 
-
+    @Column(columnDefinition = "TEXT")
     private String description;
 
-    @Column(name = "created_at")
+    @Column(name = "created_at", updatable = false)
     private Instant createdAt;
 
     @Column(name = "updated_at")
     private Instant updatedAt;
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = Instant.now();
+        this.updatedAt = Instant.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = Instant.now();
+    }
 
 }
