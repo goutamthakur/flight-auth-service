d8b1ff245bb990b3dd56e6de147f4b0e31b2a3b5
goutamthakur <goutamthakur2508@gmail.com>
2025-11-12 23:41:31 +0530

    feat: signup till password hashing logic completed

--- src/main/java/com/goutamthakur/flight/auth/application/AuthService.java
+++ src/main/java/com/goutamthakur/flight/auth/application/AuthService.java
@@ -1,8 +1,19 @@
 package com.goutamthakur.flight.auth.application;
 
+import com.goutamthakur.flight.auth.common.exception.AppException;
+import com.goutamthakur.flight.auth.domain.model.User;
+import com.goutamthakur.flight.auth.domain.port.OtpStorePort;
+import com.goutamthakur.flight.auth.domain.port.UserRepositoryPort;
+import com.goutamthakur.flight.auth.domain.service.OtpCodeGenerator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLOutput;
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class AuthService {
     // signup
     // Input validation if error return 400
@@ -11,8 +22,24 @@ public class AuthService {
     // Generate 6 digit otp
     // Send an email for verification
 
+    private final UserRepositoryPort userRepositoryPort;
+    private final OtpStorePort otpStorePort;
+    private final OtpCodeGenerator otpCodeGenerator;
+
+
     public String signUp(String email, String password){
-        return "Hello i am sign up";
+        Optional<User> existingUser = userRepositoryPort.findByEmail(email);
+        if(existingUser.isPresent()){
+           throw new AppException("Email is already registered", HttpStatus.BAD_REQUEST);
+        }
+//        String passwordHash =
+        // Password hashing logic pending
+        User newUser = userRepositoryPort.createUser(email, password);
+
+        String otp = otpCodeGenerator.generate(6);
+
+
+        return "you bro" ;
     }
 
     // verify otp
